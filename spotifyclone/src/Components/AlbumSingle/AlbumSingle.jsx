7fad4b5c790c952b6165beefb9b03dfed4410bbd
ritik337 <59756227+ritik337@users.noreply.github.com>
2023-12-14 14:51:54 +0530

    this is normal commit

---
+++ spotifyclone/src/Components/AlbumSingle/AlbumSingle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AlbumSingle = () => {
+  return (
+    <div>AlbumSingle</div>
+  )
+}
+
+export default AlbumSingle
---
+++ spotifyclone/src/Components/NotificationList/NotificationList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotificationList = () => {
+  return (
+    <div>NotificationList</div>
+  )
+}
+
+export default NotificationList
